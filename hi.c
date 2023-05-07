bf922407d893c46f05e00eebdca994c1c3893489
TomasNagy <tomi.nagy@outlook.com>
2023-05-07 20:29:07 +0200

    Add comments

--- hi.c
+++ hi.c
@@ -3,7 +3,19 @@
 
 int main(void)
 {
-	string s = "HELLO!";
+	//Create a string array 
+	string words[2];
 
-	printf("%i %i %i %i %i %i %i", s[0], s[1], s[2], s[3], s[4], s[5], s[6]);
+	//Set the first index of the array to HI!
+	words[0] = "HI!";
+	//Set the second index of the array to BYE!
+	words[1] = "BYE!";
+
+	//A string reprezents an array of characters, 
+	//we can print each character out with simple indexing
+	//There is a special character at the end of each string, that is a NUL character (ASCII 0 value)
+	//and the printf function is implemented in a way to detect the NUL character
+	//When we use string, this NUL character is automatically added to the end of the string 
+	printf("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
+	printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
 }
