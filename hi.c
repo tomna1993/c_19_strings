d9a690afba46adc332d90451ffbd33d4ae5fe17c
TomasNagy <tomi.nagy@outlook.com>
2023-05-07 20:15:01 +0200

    Print string characters as array, show the ASCII code of the characters

--- hi.c
+++ hi.c
@@ -1,10 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-	char c1 = 'H';
-	char c2 = 'I';
-	char c3 = '!';
+	string s = "HELLO!";
 
-	printf("%c%c%c", c1, c2, c3);
+	printf("%i %i %i %i %i %i %i", s[0], s[1], s[2], s[3], s[4], s[5], s[6]);
 }
